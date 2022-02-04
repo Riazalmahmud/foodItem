0bcfc9e1ca25b04bb0ffe713bce7d0b4b2d2051b
Riazalmahmud <riazalmahmud041@gmail.com>
2022-02-04 11:04:04 +0600

    home page

--- src/MainPage/Home/Home.js
+++ src/MainPage/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import health from "../../image/service/health-bottle-cut.jpg";
+import pop_smoothie_04 from "../../image/product/pop_smoothie_04-720x1030.jpg";
+import avocado from "../../image/product/avocado.png";
 import "./Home.css";
 import { GiBurningTree } from "react-icons/gi";
 import { BiDish } from "react-icons/bi";
@@ -35,9 +37,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-
       {/* Only  food service */}
-
       <div className="container-fluid my-5 py-5 service-section">
         <div className="row">
           <div className="col-lg-3 col-md-3 col-6 service-style">
@@ -94,6 +94,96 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* container section end */}
+
+      {/* Popular Smoothies start  */}
+      <div className="container my-5 py-5 popular-title">
+        <h1 className="healthy-title py-3 ">Popular Smoothies</h1>
+        <div className="row">
+          <div
+            className="col-lg-3 col-md-3 col-6 service-style popular-product"
+            id="popular-product-rgba"
+          >
+            <div className="bg-rgba"></div>
+            <div className="product-info">
+              <h5 className="service-title fs-5">Pineapple Splash</h5>
+              <p>Pineapple, mango & min</p>
+              <button type="button" className="btn btn-danger fw-bold ">
+                Add to cart
+              </button>
+            </div>
+          </div>
+          <div
+            className="col-lg-3 col-md-3 col-6 service-style popular-product02"
+            id="popular-product-rgba"
+          >
+            <div className="bg-rgba"></div>
+            <div className="product-info">
+              <h5 className="service-title fs-5">Watermelon Cooler</h5>
+              <p>Watermelon, lime and apple</p>
+              <button type="button" className="btn btn-danger fw-bold ">
+                Add to cart
+              </button>
+            </div>
+          </div>
+          <div
+            className="col-lg-3 col-md-3 col-6 service-style popular-product03"
+            id="popular-product-rgba"
+          >
+            <div className="bg-rgba"></div>
+            <div className="product-info">
+              <h5 className="service-title fs-5">Violet Banana</h5>
+              <p>Blackberry, banana & kiwi</p>
+              <button type="button" className="btn btn-danger fw-bold ">
+                Add to cart
+              </button>
+            </div>
+          </div>
+          <div
+            className="col-lg-3 col-md-3 col-6 service-style popular-product04"
+            id="popular-product-rgba"
+          >
+            <div className="bg-rgba"></div>
+            <div className="product-info">
+              <h5 className="service-title fs-5">Pineapple Splash</h5>
+              <p>Pineapple, mango & min</p>
+              <button type="button" className="btn btn-danger fw-bold ">
+                add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* container section end */}
+      <div className="container-fluid organic-food-style">
+        <div className="organic-food">
+          <img className="img-fluid organic-food" src={avocado} alt="" />
+        </div>
+      </div>
+      {/* container organic section start */}
+      <div class="container my-5 py-5">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <div class="col-lg-3 col-md-3 col-6">
+            <div class="p-3 border bg-light">
+              Row col-lg-3 col-md-3 col-6umn
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-6">
+            <div class="p-3 border bg-light">
+              Row col-lg-3 col-md-3 col-6umn
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-6">
+            <div class="p-3 border bg-light">
+              Row col-lg-3 col-md-3 col-6umn
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-6">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+        </div>
+      </div>
+      {/* container section end */}
     </div>
   );
 };
